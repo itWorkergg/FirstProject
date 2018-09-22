d9493385ef774c5c26b61a38827c3132b84806a7
zdy <908843012@qq.com>
2018-09-22 23:18:14 +0800

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class HelloWorld {
+
+    public static void main(String[] args)
+    {
+        System.out.println("hello world!");
+        ArrayList arraylist = new ArrayList();
+    }
+
+    public void method()
+    {}
+}
---
+++ src/HelloWorld1.java
@@ -0,0 +1,6 @@
+/**
+ * @author zdy
+ * @create 2018-09-22 17:33
+ */
+public class HelloWorld1 {
+}
